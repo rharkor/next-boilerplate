abaa646cad3619db9aa4ab0b12c6ef7ad4adba68
rharkor <louis@huort.com>
2023-07-25 19:30:55 +0200

    feat: get device

---
+++ src/components/profile/get-device-icon.tsx
@@ -0,0 +1,48 @@
+import { Icons } from "../icons"
+
+export default function GetDeviceIcon({ name }: { name?: string }) {
+  if (!name) {
+    return <Icons.desktop />
+    // return 'unknown-logo';
+  }
+
+  if (
+    [
+      "Android",
+      "CyanogenMod",
+      "LineageOS",
+      "MIUI",
+      "OxygenOS",
+      "One UI",
+      "Windows Phone",
+      "Windows Mobile",
+      "ColorOS",
+    ].includes(name)
+  ) {
+    // return 'android-logo';
+    return <Icons.mobile />
+  }
+
+  if (["iOS", "iPadOS"].includes(name)) {
+    // return 'ios-logo';
+    return <Icons.mobile />
+  }
+
+  if (["Windows"]) {
+    // return 'windows-logo';
+    return <Icons.desktop />
+  }
+
+  if (["Arch", "CentOS", "Debian", "Fedora", "Gentoo", "Linux Mint", "openSUSE", "Red Hat", "Ubuntu"].includes(name)) {
+    // return 'linux-logo';
+    return <Icons.desktop />
+  }
+
+  if (["macOS", "OS X"].includes(name)) {
+    // return 'macos-logo';
+    return <Icons.desktop />
+  }
+
+  // return 'unknown-logo';
+  return <Icons.desktop />
+}
