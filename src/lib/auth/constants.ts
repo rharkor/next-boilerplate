2e6be6f7a084fcf118d4bd44b6d279b71d505c43
rharkor <louis@huort.com>
2023-07-25 19:38:22 +0200

    fix: jwt duration

--- src/lib/auth/constants.ts
+++ src/lib/auth/constants.ts
@@ -1,4 +1,4 @@
-export const JWT_MAX_AGE = 30 * 24 * 30 * 60 // 30 days
+export const JWT_MAX_AGE = 30 * 24 * 60 * 60 // 30 days
 
 export const authRoutes = {
   signIn: ["/sign-in", "/login", "/signin"],
