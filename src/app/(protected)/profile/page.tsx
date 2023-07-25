97af26825b43df6e13dd8aa799787235fae56b07
rharkor <louis@huort.com>
2023-07-25 19:29:21 +0200

    refactor: see details toggle

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth"
 import SignoutButton from "@/components/auth/sign-out-button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { nextAuthOptions } from "@/lib/auth"
+import SeeDetailsToggle from "./see-details-toggle"
 
 export default async function Profile() {
   const session = await getServerSession(nextAuthOptions)
@@ -9,19 +10,26 @@ export default async function Profile() {
 
   return (
     <main className="container flex flex-1 items-center justify-center">
-      <Card className="mx-auto">
-        <CardHeader>
-          <CardTitle>Profile</CardTitle>
-        </CardHeader>
-        <CardContent>
-          {!user ? <p>Loading...</p> : <pre className="rounded bg-muted p-2">{JSON.stringify(session, null, 2)}</pre>}
-        </CardContent>
-        <CardFooter>
-          <div className="ml-auto">
-            <SignoutButton />
-          </div>
-        </CardFooter>
-      </Card>
+      <div className="">
+        <Card className="relative z-10 mx-auto max-w-full">
+          <CardHeader>
+            <CardTitle>Profile</CardTitle>
+          </CardHeader>
+          <CardContent>
+            {!user ? (
+              <p>Loading...</p>
+            ) : (
+              <pre className="overflow-auto rounded bg-muted p-2">{JSON.stringify(session, null, 2)}</pre>
+            )}
+          </CardContent>
+          <CardFooter>
+            <div className="ml-auto">
+              <SignoutButton />
+            </div>
+          </CardFooter>
+        </Card>
+        <SeeDetailsToggle />
+      </div>
     </main>
   )
 }
