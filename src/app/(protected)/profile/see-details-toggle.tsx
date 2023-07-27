0f8320081acf2f38b48a08143a593aebacf6b76f
rharkor <louis@huort.com>
2023-07-27 14:59:22 +0200

    refactor: settings order

--- src/app/(protected)/profile/see-details-toggle.tsx
+++ src/app/(protected)/profile/see-details-toggle.tsx
@@ -7,10 +7,10 @@ export default function SeeDetailsToggle() {
     <div className="-mt-4 rounded-b-lg border bg-card p-2 pt-6 text-muted-foreground shadow-sm">
       <Accordion type="single" collapsible>
         <AccordionItem value="session-toggle" className="border-b-0">
-          <AccordionTrigger className="flex-none gap-1">See details</AccordionTrigger>
+          <AccordionTrigger className="flex-none gap-1 p-0">See details</AccordionTrigger>
           <AccordionContent>
-            <ProfileDetails />
             <UserActiveSessions />
+            <ProfileDetails />
           </AccordionContent>
         </AccordionItem>
       </Accordion>
--- src/components/profile/sessions/user-active-sessions.tsx
+++ src/components/profile/sessions/user-active-sessions.tsx
@@ -2,7 +2,7 @@ import SessionsTable from "./sessions-table"
 
 export default function UserActiveSessions() {
   return (
-    <section className="mt-4 p-2 text-foreground">
+    <section className="p-2 text-foreground">
       <header>
         <h3 className="text-lg font-medium">Logged in devices</h3>
         <p className="text-sm text-muted-foreground">Manage your logged in devices here.</p>
