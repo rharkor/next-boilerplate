bc69b6409d3c55a16ce96983864d64d9a8006f34
rharkor <louis@huort.com>
2023-07-28 12:13:15 +0200

    feat: button test

---
+++ src/components/ui/button.test.tsx
@@ -0,0 +1,46 @@
+import "isomorphic-fetch"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { Button } from "./button"
+
+describe("Button", () => {
+  it("renders a button with the correct text", () => {
+    render(<Button>Click me</Button>)
+    const button = screen.getByRole("button", { name: /click me/i })
+    expect(button).toBeInTheDocument()
+  })
+
+  it("calls the onClick function when clicked", () => {
+    const handleClick = jest.fn()
+    render(<Button onClick={handleClick}>Click me</Button>)
+    const button = screen.getByRole("button", { name: /click me/i })
+    userEvent.click(button)
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+
+  it("disables the button when isLoading is true", () => {
+    render(<Button isLoading>Click me</Button>)
+    const button = screen.getByRole("button", { name: /click me/i })
+    expect(button).toBeDisabled()
+  })
+
+  it("disables the button when disabledWhileLoading is true and isLoading is true", () => {
+    render(
+      <Button isLoading disabledWhileLoading>
+        Click me
+      </Button>
+    )
+    const button = screen.getByRole("button", { name: /click me/i })
+    expect(button).toBeDisabled()
+  })
+
+  it("does not disable the button when disabledWhileLoading is false and isLoading is true", () => {
+    render(
+      <Button isLoading disabledWhileLoading={false}>
+        Click me
+      </Button>
+    )
+    const button = screen.getByRole("button", { name: /click me/i })
+    expect(button).not.toBeDisabled()
+  })
+})
