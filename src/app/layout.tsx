748d58f8f887acb1c2d7bb0363d3e97161719771
rharkor <louis@huort.com>
2023-07-28 11:14:35 +0200

    feat: add theme switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Metadata } from "next"
 import React from "react"
 import "./globals.css"
 import { NextAuthProvider } from "@/components/auth/provider"
+import { ThemeProvider } from "@/components/theme/theme-provider"
+import { ThemeSwitch } from "@/components/theme/theme-switch"
 import { Toaster } from "@/components/ui/toaster"
 import QueryClientProvider from "@/contexts/query-provider"
 
@@ -16,8 +18,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="flex min-h-screen flex-col">
         <NextAuthProvider>
           <QueryClientProvider>
-            {children}
-            <Toaster />
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <div className="fixed right-2 top-2 z-10">
+                <ThemeSwitch />
+              </div>
+              {children}
+              <Toaster />
+            </ThemeProvider>
           </QueryClientProvider>
         </NextAuthProvider>
       </body>
