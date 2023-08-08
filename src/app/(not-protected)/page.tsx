9367185d1b7336597843c3bb9fac0ea99e3fb5c3
rharkor <louis@huort.com>
2023-08-08 21:32:10 +0200

    refactor: move theme switch

---
+++ src/app/(not-protected)/page.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link"
+import { ThemeSwitch } from "@/components/theme/theme-switch"
+import { buttonVariants } from "@/components/ui/button"
+import { authRoutes } from "@/lib/auth/constants"
+
+export default function Home() {
+  return (
+    <>
+      <main className="container flex flex-1 flex-col items-center justify-center gap-3">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+        <nav className="flex flex-col items-center justify-center">
+          <ul className="flex flex-row items-center justify-center gap-2">
+            <li>
+              <Link href={authRoutes.signIn[0]} className={buttonVariants({ variant: "ghost" })}>
+                Sign In
+              </Link>
+            </li>
+            <li>
+              <Link href={authRoutes.signUp[0]} className={buttonVariants({ variant: "ghost" })}>
+                Sign Up
+              </Link>
+            </li>
+            <li>
+              <Link href="/profile" className={buttonVariants({ variant: "ghost" })}>
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </main>
+      <div className="fixed right-2 top-2 z-10">
+        <ThemeSwitch />
+      </div>
+    </>
+  )
+}
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,15 @@
 import requireAuth from "@/components/auth/require-auth"
+import { ThemeSwitch } from "@/components/theme/theme-switch"
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   await requireAuth("/profile")
 
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      <div className="fixed right-2 top-2 z-10">
+        <ThemeSwitch />
+      </div>
+    </>
+  )
 }
