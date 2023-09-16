e3a50cbcb8a2d0a50df705e9bad8eda107cab4cf
rharkor <louis@huort.com>
2023-09-16 22:24:40 +0200

    refactor: change color on hover

--- src/components/auth/login-user-auth-form.tsx
+++ src/components/auth/login-user-auth-form.tsx
@@ -134,7 +134,7 @@ export function LoginUserAuthForm({ dictionary, searchParams, ...props }: UserAu
             name="password"
           />
         </div>
-        <Link className="ml-auto text-sm text-muted-foreground hover:text-white" href={"/forgot-password"}>
+        <Link className="ml-auto text-sm text-muted-foreground hover:text-primary" href={"/forgot-password"}>
           {dictionary.forgotPassword}
         </Link>
         <Button type="submit" isLoading={isLoading}>
