4e2bd74e86899a75a4802f16ef8f424cba6583a6
rharkor <louis@huort.com>
2023-09-28 14:45:50 +0200

    style: fixes

--- src/components/need-save-popup.tsx
+++ src/components/need-save-popup.tsx
@@ -16,13 +16,13 @@ export type INeedSavePopupProps = {
 export default function NeedSavePopup({ show, onReset, onSave, text, isSubmitting, dictionary }: INeedSavePopupProps) {
   return (
     <div
-      className={cn("fixed bottom-0 z-50 mx-0 overflow-hidden pb-4", {
+      className={cn("fixed bottom-0 left-1 right-1 z-50 mx-0 overflow-hidden pb-4 lg:left-2 lg:right-2", {
         "pointer-events-none": !show,
       })}
     >
       <div
         className={cn(
-          "flex w-max translate-y-full flex-row items-center justify-center space-x-4 rounded border border-card-foreground/40 bg-card px-4 py-2 opacity-0 transition-all duration-300",
+          "m-auto flex w-max translate-y-full flex-col items-center justify-center space-y-2 rounded border border-card-foreground/40 bg-card px-4 py-2 opacity-0 transition-all duration-300 lg:flex-row lg:space-x-4 lg:space-y-0",
           {
             "translate-y-0 animate-[bounce-up_1s_ease-out] opacity-100": show,
           }
