a26f35ff2cb1b482813c3eecf224e280ed65b650
rharkor <louis@huort.com>
2023-12-09 18:57:27 +0100

    feat: full docker compose

--- packages/landing/next.config.mjs
+++ packages/landing/next.config.mjs
@@ -12,36 +12,6 @@ const config = {
       { source: "/api/ping", destination: "/api/health" },
     ]
   },
-  redirects() {
-    //? Permanent redirects only in prod env
-    return [
-      { source: "/signin", destination: "/sign-in", permanent: process.env.ENV === "production" ? true : false },
-      { source: "/login", destination: "/sign-in", permanent: process.env.ENV === "production" ? true : false },
-      { source: "/signup", destination: "/sign-up", permanent: process.env.ENV === "production" ? true : false },
-      { source: "/register", destination: "/sign-up", permanent: process.env.ENV === "production" ? true : false },
-
-      {
-        source: "/:lang/signin",
-        destination: "/:lang/sign-in",
-        permanent: process.env.ENV === "production" ? true : false,
-      },
-      {
-        source: "/:lang/login",
-        destination: "/:lang/sign-in",
-        permanent: process.env.ENV === "production" ? true : false,
-      },
-      {
-        source: "/:lang/signup",
-        destination: "/:lang/sign-up",
-        permanent: process.env.ENV === "production" ? true : false,
-      },
-      {
-        source: "/:lang/register",
-        destination: "/:lang/sign-up",
-        permanent: process.env.ENV === "production" ? true : false,
-      },
-    ]
-  },
 }
 
 export default config
--- packages/landing/src/app/[lang]/not-found.tsx
+++ packages/landing/src/app/[lang]/not-found.tsx
@@ -1,14 +1,11 @@
 import React from "react"
-
-import { Button, Link } from "@nextui-org/react"
+import Link from "next/link"
 
 export default async function Page404MatchAll() {
   return (
     <main className="container m-auto flex min-h-screen flex-1 flex-col items-center justify-center gap-3">
       <h1 className="text-4xl font-bold">Not found</h1>
-      <Button as={Link} href="/" color="primary" variant="flat">
-        Home
-      </Button>
+      <Link href="/">Home</Link>
     </main>
   )
 }
