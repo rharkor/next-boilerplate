45bf0f8b7a290701f7baf0cbf9c6407f225a7331
rharkor <louis@huort.com>
2023-12-09 19:34:58 +0100

    refactor: landing

--- packages/landing/src/app/[lang]/not-found.tsx
+++ packages/landing/src/app/[lang]/not-found.tsx
@@ -5,7 +5,9 @@ export default async function Page404MatchAll() {
   return (
     <main className="container m-auto flex min-h-screen flex-1 flex-col items-center justify-center gap-3">
       <h1 className="text-4xl font-bold">Not found</h1>
-      <Link href="/">Home</Link>
+      <Link href="/" className="hover:text-blue-500">
+        Home
+      </Link>
     </main>
   )
 }
--- packages/landing/src/app/[lang]/page.tsx
+++ packages/landing/src/app/[lang]/page.tsx
@@ -2,6 +2,8 @@ import { Locale } from "i18n-config"
 
 import { getDictionary } from "@/lib/langs"
 
+import Blob from "./blob"
+
 export default async function Home({
   params: { lang },
 }: {
@@ -11,5 +13,14 @@ export default async function Home({
 }) {
   const dictionary = await getDictionary(lang)
 
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">{dictionary.home}</main>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 md:p-24">
+      <div className="absolute left-0 top-0 h-full w-full overflow-hidden">
+        <Blob />
+      </div>
+      <h1 className="animate-fade-in-scale relative z-10 rounded-lg p-5 text-center text-4xl font-bold text-white/80 md:text-6xl lg:text-8xl">
+        {dictionary.home.title}
+      </h1>
+    </main>
+  )
 }
