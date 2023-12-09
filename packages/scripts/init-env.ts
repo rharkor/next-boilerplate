148a07adde0e48faa9ff8404a452442126940974
rharkor <louis@huort.com>
2023-12-09 21:33:53 +0100

    fix: script

---
+++ packages/scripts/init-env.ts
@@ -0,0 +1,12 @@
+import chalk from "chalk"
+import { envSetup } from "env-setup"
+import { exit } from "node:process"
+
+async function main() {
+  console.log(chalk.blue('Starting the "env setup" script...'))
+  await envSetup()
+  console.log(chalk.green("Done!"))
+  exit(0)
+}
+
+main()
