dd2b1a09b61305c1d31e62172a1de0232c4e9791
rharkor <louis@huort.com>
2024-02-23 08:35:06 +0100

    refactor: sign-in

--- packages/app/src/lib/auth/handle-sign.ts
+++ packages/app/src/lib/auth/handle-sign.ts
@@ -46,8 +46,10 @@ export const handleSignIn = async ({
       if (!res?.error) {
         logger.debug("Sign in successful pushing to", callbackUrl)
         router.push(callbackUrl)
+        /*
         //? Refreshing the router is necessary due to next.js client cache, see: https://nextjs.org/docs/app/building-your-application/caching
-        router.refresh()
+        // router.refresh()
+        */
         resolve()
       } else {
         if (res.error === "OTP_REQUIRED") {
