58522d83d1ad9b6049efa2924131bc0609ec1722
rharkor <louis@huort.com>
2024-03-11 21:41:12 +0100

    refactor: script

--- packages/scripts/init.ts
+++ packages/scripts/init.ts
@@ -1,8 +1,13 @@
 import chalk from "chalk";
+import { completeInitialisation } from "complete-initialisation";
 import { config } from "dotenv";
+import { envSetup } from "env-setup";
+import * as fs from "fs/promises";
 import { modulesSelection } from "modules-selection";
 import { exit } from "node:process";
+import { packagesSelection } from "packages-selection";
 import * as path from "path";
+import { replaceTokens } from "replace-tokens";
 import * as url from "url";
 
 import { logger } from "@next-boilerplate/lib/logger";
@@ -13,39 +18,38 @@ const rootPath = path.join(__dirname, "..");
 config();
 
 async function main() {
-  // TODO Uncomment
-  // const alreadyInitialized = await fs
-  //   .access(path.join(rootPath, "scripts", ".init-todo"))
-  //   .then(() => false)
-  //   .catch(() => true);
+  const alreadyInitialized = await fs
+    .access(path.join(rootPath, "scripts", ".init-todo"))
+    .then(() => false)
+    .catch(() => true);
 
-  // if (!alreadyInitialized) {
-  //   logger.log(chalk.green("Welcome to the init script!"));
-  //   logger.log(chalk.blue('Starting the "replace tokens" script...'));
-  //   await replaceTokens();
-  //   logger.log(chalk.green("Done!"));
-  // } else {
-  //   logger.log(chalk.yellow("Skipping replaceTokens()"));
-  // }
+  if (!alreadyInitialized) {
+    logger.log(chalk.green("Welcome to the init script!"));
+    logger.log(chalk.blue('Starting the "replace tokens" script...'));
+    await replaceTokens();
+    logger.log(chalk.green("Done!"));
+  } else {
+    logger.log(chalk.yellow("Skipping replaceTokens()"));
+  }
 
-  // if (!alreadyInitialized) {
-  //   logger.log(chalk.blue('Starting the "packages selection" script...'));
-  //   await packagesSelection();
-  //   logger.log(chalk.green("Done!"));
-  // } else {
-  //   logger.log(chalk.gray("Skipping packagesSelection()"));
-  // }
+  if (!alreadyInitialized) {
+    logger.log(chalk.blue('Starting the "packages selection" script...'));
+    await packagesSelection();
+    logger.log(chalk.green("Done!"));
+  } else {
+    logger.log(chalk.gray("Skipping packagesSelection()"));
+  }
 
   logger.log(chalk.blue('Starting the "modules selection" script...'));
   await modulesSelection();
   logger.log(chalk.green("Done!"));
 
-  // logger.log(chalk.blue('Starting the "env setup" script...'));
-  // await envSetup();
-  // logger.log(chalk.green("Done!"));
+  logger.log(chalk.blue('Starting the "env setup" script...'));
+  await envSetup();
+  logger.log(chalk.green("Done!"));
 
-  // logger.log(chalk.blue('Starting the "complete initialisation" script...'));
-  // await completeInitialisation();
+  logger.log(chalk.blue('Starting the "complete initialisation" script...'));
+  await completeInitialisation();
 
   exit(0);
 }
