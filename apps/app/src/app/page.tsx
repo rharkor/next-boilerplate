0f25871a23d7c3bda374472f9ad74769be1a0f12
rharkor <louis@huort.com>
2024-03-13 17:01:53 +0100

    refactor: terraform

---
+++ apps/app/src/app/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@nextui-org/react"
+
+export default function Page() {
+  return <Button>Hello</Button>
+}
---
+++ architecture/ecs-project/terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.36"
+    }
+  }
+
+  #? Variables are not available in the backend block
+  backend "s3" {
+    bucket = "next-boilerplate-terra-config"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+module "project" {
+  source = "./modules/ec2-project"
+
+
+  projectName                  = var.project_name
+  region                       = var.aws_region
+  task_definitions             = var.task_definitions
+  ecs_task_execution_role_arn  = var.ecs_task_execution_role_arn
+  ecs_task_execution_role_name = var.ecs_task_execution_role_name
+  public_ec2_key               = file("keys/id_rsa.pub")
+  user_data                    = file("user_data.sh")
+}
+
+
+
+# module "project" {
+#   source = "./modules/fargate-project"
+
+
+#   projectName                 = var.project_name
+#   region                      = var.aws_region
+#   task_definitions            = var.task_definitions
+#   ecs_task_execution_role_arn = var.ecs_task_execution_role_arn
+# }
