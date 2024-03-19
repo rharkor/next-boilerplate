9fd51d14564f9a25874f0e265c6421176217b9fe
rharkor <louis@huort.com>
2024-03-19 09:55:31 +0100

    fix: prisma logging

--- apps/cron/src/lib/prisma.ts
+++ apps/cron/src/lib/prisma.ts
@@ -4,8 +4,6 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["warn", "error"],
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
