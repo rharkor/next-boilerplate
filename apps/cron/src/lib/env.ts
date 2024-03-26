b19ec9cb80674d2f43b3e347b0df4f1abac59404
rharkor <louis@huort.com>
2024-03-26 13:54:06 +0100

    refactor: remove dotenv and logger from env.ts

--- apps/cron/src/lib/env.ts
+++ apps/cron/src/lib/env.ts
@@ -1,11 +1,6 @@
-import { config } from "dotenv"
 import { z } from "zod"
 
 import { createEnv } from "@t3-oss/env-core"
-
-if (!process.env.ENV) {
-  config()
-}
 
 export const env = createEnv({
   server: {
