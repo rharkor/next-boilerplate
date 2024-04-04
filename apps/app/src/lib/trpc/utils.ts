a01833b473262953ce84301cdc6272423f6297d1
rharkor <louis@huort.com>
2024-04-04 13:44:01 +0200

    refactor: getUrl

--- apps/app/src/lib/trpc/utils.ts
+++ apps/app/src/lib/trpc/utils.ts
@@ -1,18 +1,8 @@
 import { AppRouter } from "@/api/_app"
-import { env } from "@/lib/env"
-import { logger } from "@next-boilerplate/lib/logger"
 import { inferRouterOutputs } from "@trpc/server"
 
-function getBaseUrl() {
-  if (typeof window !== "undefined") return ""
-  if (env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
-  if (env.NEXT_PUBLIC_BASE_URL) return env.NEXT_PUBLIC_BASE_URL
-  logger.warn("No NEXT_PUBLIC_BASE_URL or VERCEL_URL found, using http://localhost:3000")
-  return "http://localhost:3000"
-}
-
 export function getUrl() {
-  return getBaseUrl() + "/api/trpc"
+  return "/api/trpc"
 }
 
 /**
