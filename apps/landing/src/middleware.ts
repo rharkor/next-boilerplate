1a8d4f45c837cd8f52bdf6ad39a4b1f3e38e999c
rharkor <louis@huort.com>
2024-04-15 09:23:48 +0200

    refactor: middleware doc

--- apps/landing/src/middleware.ts
+++ apps/landing/src/middleware.ts
@@ -74,6 +74,14 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|public).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|public).*)",
+  ],
 }
