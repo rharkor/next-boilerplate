b7c56b266a58afad3cff6a186eeb4697738c7921
rharkor <louis@huort.com>
2024-04-22 14:19:48 +0200

    refactor: error responsive

--- apps/app/src/app/[lang]/error.tsx
+++ apps/app/src/app/[lang]/error.tsx
@@ -5,7 +5,7 @@ import { Button } from "@nextui-org/react"
 export default function ErrorPage({ reset }: { reset: () => void }) {
   return (
     <main className="container m-auto flex min-h-screen flex-1 flex-col items-center justify-center gap-3">
-      <h1 className="text-4xl font-bold">Error something went wrong</h1>
+      <h1 className="text-center text-xl font-bold lg:text-4xl">Error something went wrong</h1>
       <Button onPress={reset} color="primary" variant="flat">
         Try again
       </Button>
