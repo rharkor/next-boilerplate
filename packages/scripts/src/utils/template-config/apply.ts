aa9ffc1764ef771c6bf52d0013314be99a5ca6d5
rharkor <louis@huort.com>
2024-09-04 08:46:50 +0200

    refactor: task

--- packages/scripts/src/utils/template-config/apply.ts
+++ packages/scripts/src/utils/template-config/apply.ts
@@ -10,29 +10,38 @@ export const applyConfigurationTask = async ({
   configFileName,
   pluginsDirectory,
   root,
+  noTask,
 }: {
   pluginsDirectory: string
   configFileName: string
   root: string
+  noTask: boolean
 }) => {
-  const applyConfigTask = await task.startTask({
-    name: "Apply template config... 🧰",
-  })
+  let applyConfigTask: null | Awaited<ReturnType<(typeof task)["startTask"]>> = null
+  if (!noTask) {
+    applyConfigTask = await task.startTask({
+      name: "Apply template config... 🧰",
+    })
+  }
 
   //* Check if the plugins directory exists
-  applyConfigTask.log("Checking if the plugins directory exists")
+  if (applyConfigTask) applyConfigTask.log("Checking if the plugins directory exists")
+  else logger.info("Checking if the plugins directory exists")
   if (!(await fs.exists(pluginsDirectory))) {
-    applyConfigTask.error(`The plugins directory doesn't exist at ${pluginsDirectory}`)
-    applyConfigTask.stop()
+    if (applyConfigTask) applyConfigTask.error(`The plugins directory doesn't exist at ${pluginsDirectory}`)
+    else logger.error(`The plugins directory doesn't exist at ${pluginsDirectory}`)
+    applyConfigTask?.stop()
     throw new Error(`The plugins directory doesn't exist at ${pluginsDirectory}`)
   }
 
   //* Retrieve config
-  applyConfigTask.log("Retrieving the config")
+  if (applyConfigTask) applyConfigTask.log("Retrieving the config")
+  else logger.info("Retrieving the config")
   const configPath = !root.endsWith(configFileName) ? path.join(root, configFileName) : root
   if (!(await fs.exists(configPath))) {
-    applyConfigTask.error(`The config file ${configPath} doesn't exist`)
-    applyConfigTask.stop()
+    if (applyConfigTask) applyConfigTask.error(`The config file ${configPath} doesn't exist`)
+    else logger.error(`The config file ${configPath} doesn't exist`)
+    applyConfigTask?.stop()
     throw new Error(`The config file ${configPath} doesn't exist`)
   }
 
@@ -40,21 +49,24 @@ export const applyConfigurationTask = async ({
   try {
     configSchema.parse(config)
   } catch (error) {
-    applyConfigTask.error(`The config file ${configPath} is not valid`)
-    applyConfigTask.stop()
+    if (applyConfigTask) applyConfigTask.error(`The config file ${configPath} is not valid`)
+    else logger.error(`The config file ${configPath} is not valid`)
+    applyConfigTask?.stop()
     logger.error(error)
     throw error
   }
 
   //* Apply plugins
-  applyConfigTask.log("Applying plugins")
+  if (applyConfigTask) applyConfigTask.log("Applying plugins")
+  else logger.info("Applying plugins")
 
   for (const plugin of config.plugins) {
     const pluginName = typeof plugin === "string" ? plugin : plugin.name
     const pluginPath = path.join(pluginsDirectory, pluginName)
     if (!(await fs.exists(pluginPath))) {
-      applyConfigTask.error(`The plugin ${pluginName} doesn't exist at ${pluginPath}`)
-      applyConfigTask.stop()
+      if (applyConfigTask) applyConfigTask.error(`The plugin ${pluginName} doesn't exist at ${pluginPath}`)
+      else logger.error(`The plugin ${pluginName} doesn't exist at ${pluginPath}`)
+      applyConfigTask?.stop()
       throw new Error(`The plugin ${pluginName} doesn't exist at ${pluginPath}`)
     }
     const pluginConfigPath = path.join(pluginPath, configFileName)
@@ -62,16 +74,20 @@ export const applyConfigurationTask = async ({
     try {
       pluginConfigSchema.parse(pluginConfig)
     } catch (error) {
-      applyConfigTask.error(`The plugin config file ${pluginConfigPath} is not valid. Contact developer`)
-      applyConfigTask.stop()
+      if (applyConfigTask)
+        applyConfigTask.error(`The plugin config file ${pluginConfigPath} is not valid. Contact developer`)
+      else logger.error(`The plugin config file ${pluginConfigPath} is not valid. Contact developer`)
+      applyConfigTask?.stop()
       logger.error(error)
       throw error
     }
 
     //? Check if the plugin config exists
     if (!(await fs.exists(pluginConfigPath))) {
-      applyConfigTask.error(`The plugin config for ${pluginName} doesn't exist at ${pluginConfigPath}`)
-      applyConfigTask.stop()
+      if (applyConfigTask)
+        applyConfigTask.error(`The plugin config for ${pluginName} doesn't exist at ${pluginConfigPath}`)
+      else logger.error(`The plugin config for ${pluginName} doesn't exist at ${pluginConfigPath}`)
+      applyConfigTask?.stop()
       throw new Error(`The plugin config for ${pluginName} doesn't exist at ${pluginConfigPath}`)
     }
 
@@ -81,15 +97,17 @@ export const applyConfigurationTask = async ({
     for (const relativeDestinationPath of relativeDestinationPaths) {
       const destinationPath = path.join(root, relativeDestinationPath)
       if (await fs.exists(destinationPath)) {
-        applyConfigTask.error(`A file/folder already exists at the destination ${destinationPath}`)
-        applyConfigTask.stop()
+        if (applyConfigTask) applyConfigTask.error(`A file/folder already exists at the destination ${destinationPath}`)
+        else logger.error(`A file/folder already exists at the destination ${destinationPath}`)
+        applyConfigTask?.stop()
         throw new Error(`A file/folder already exists at the destination ${destinationPath}`)
       }
     }
   }
 
   //* Apply the plugins
-  applyConfigTask.log("Applying the plugins")
+  if (applyConfigTask) applyConfigTask.log("Applying the plugins")
+  else logger.info("Applying the plugins")
   for (const plugin of config.plugins) {
     const pluginName = typeof plugin === "string" ? plugin : plugin.name
     const pluginPath = path.join(pluginsDirectory, pluginName)
@@ -99,7 +117,8 @@ export const applyConfigurationTask = async ({
     for (const { from, to } of pluginConfig.paths) {
       const sourcePath = path.join(pluginPath, from)
       const destinationPath = path.join(root, to)
-      applyConfigTask.log(`Copying the plugin ${pluginName} to the destination ${destinationPath}`)
+      if (applyConfigTask) applyConfigTask.log(`Copying the plugin ${pluginName} to the destination ${destinationPath}`)
+      else logger.info(`Copying the plugin ${pluginName} to the destination ${destinationPath}`)
       await fs.copy(sourcePath, destinationPath)
     }
   }
@@ -108,6 +127,7 @@ export const applyConfigurationTask = async ({
   //   applyConfigTask.log("Deleting the config file")
   //   await fs.remove(configPath)
 
-  applyConfigTask.stop("The template config has been applied! 🎉")
+  if (applyConfigTask) applyConfigTask.stop("The template config has been applied! 🎉")
+  else logger.success("The template config has been applied! 🎉")
   logger.info("You can now delete the config file if you want")
 }
