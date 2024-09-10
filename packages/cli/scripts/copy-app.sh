cf65eb9ea1f235e3a47dd5b718fb7a97a6a2da21
rharkor <louis@huort.com>
2024-09-10 08:35:35 +0200

    fix: open web

--- packages/cli/scripts/copy-app.sh
+++ packages/cli/scripts/copy-app.sh
@@ -6,6 +6,7 @@ set -e
 rm -rf dist/app
 mkdir -p dist/app/.next
 cp -r ../cli-app/public dist/app/public
+cp -r ../cli-app/.next/standalone/node_modules dist/app
 cp -r ../cli-app/.next/standalone/packages/cli-app/* dist/app
 cp -r ../cli-app/.next/standalone/packages/cli-app/.next dist/app
 cp -r ../cli-app/.next/static dist/app/.next/static
--- packages/cli/src/cli/web/index.ts
+++ packages/cli/src/cli/web/index.ts
@@ -12,8 +12,12 @@ const __dirname = path.dirname(__filename) // get the name of the directory
 // const dir = path.resolve(__dirname, "../../..")
 //? Do not use ../../.. because this file will be compiled and put in dist/index.js so the path will be wrong
 const dir = __dirname
+//? Change cwd because the assets are not in the same folder
 const cwd = process.cwd()
 
+const defaultPort = 3000
+// eslint-disable-next-line no-process-env
+const resolvedPort = process.env.PORT || defaultPort.toString()
 export const openWeb = async () => {
   await logger.init()
   logger._log("Starting web interface")
@@ -23,6 +27,8 @@ export const openWeb = async () => {
       NODE_ENV: "production",
       HOSTNAME: "0.0.0.0",
       ROOT_PATH: cwd,
+      PORT: resolvedPort,
+      // CLI_REL_PATH: "../../../..",
     },
   })
   cp.stdout?.pipe(process.stdout)
@@ -30,6 +36,6 @@ export const openWeb = async () => {
   cp.on("close", (code) => {
     logger._log(`Web interface exited with code ${code}`)
   })
-  await waitPort({ host: "localhost", port: 3000, timeout: 10000, interval: 500, output: "silent" })
-  await open("http://localhost:3000")
+  await waitPort({ host: "localhost", port: parseInt(resolvedPort), timeout: 10000, interval: 500, output: "silent" })
+  await open(`http://localhost:${resolvedPort}`)
 }
